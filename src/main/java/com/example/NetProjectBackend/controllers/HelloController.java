7cf571747000543f3a346ae26eafddb67ebe7289
Tatiana000 <alice.everdeen.nya@gmail.com>
2021-11-02 13:33:44 +0200

    First commit

---
+++ src/main/java/com/example/NetProjectBackend/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.NetProjectBackend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String getHello(){
+        return "hello world";
+    }
+}
