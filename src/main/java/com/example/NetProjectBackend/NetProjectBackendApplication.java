b1e99d7571affaba485e1513be439bab22a30e74
MaksimNemoshenkov <shalun084@gmail.com>
2021-11-03 09:44:24 +0200

    test commit5

--- src/main/java/com/example/NetProjectBackend/NetProjectBackendApplication.java
+++ src/main/java/com/example/NetProjectBackend/NetProjectBackendApplication.java
@@ -8,6 +8,7 @@ public class NetProjectBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetProjectBackendApplication.class, args);
+		System.out.println("Hallo World! from dev-01");
 	}
 
 }
