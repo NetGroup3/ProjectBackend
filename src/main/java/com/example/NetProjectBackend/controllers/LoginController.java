565f9f0c8b28861b8bb5a71364d66689f55bcd2f
MaksimNemoshenkov <shalun084@gmail.com>
2021-11-15 15:26:34 +0200

    rebase UserService and add create, recovery methods

--- src/main/java/com/example/NetProjectBackend/controllers/LoginController.java
+++ src/main/java/com/example/NetProjectBackend/controllers/LoginController.java
@@ -2,35 +2,31 @@ package com.example.NetProjectBackend.controllers;
 
 import com.example.NetProjectBackend.models.User;
 import com.example.NetProjectBackend.repositories.UserRepository;
-import com.example.NetProjectBackend.services.mail.Mail;
+import com.example.NetProjectBackend.service.UserService;
 import com.example.NetProjectBackend.services.password.HashPassword;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.OffsetDateTime;
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 
 @RestController
 public class LoginController {
 
-    private final Mail mail;
     private final UserRepository userRepository;
+    private final UserService userService;
 
-    LoginController(UserRepository userRepository, Mail mail) {
-        this.mail = mail;
+
+    LoginController(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
 
     @GetMapping( path = "/login")
     public ResponseEntity<User> login(@RequestHeader(value = "Authorization") String authHeader) {
         System.out.println("login");
-        Map<String, String> returnValue = new HashMap<>();
-        returnValue.put("Authorization", authHeader);
         System.out.println(authHeader);
         String decoded = new String(Base64.getDecoder().decode(authHeader));
 
@@ -42,7 +38,7 @@ public class LoginController {
             return ResponseEntity.notFound().build();
         }
 
-        /** We compare the hash of the password from a DB and the hash entered by the user */
+        /* We compare the hash of the password from a DB and the hash entered by the user */
         if(Objects.equals(user.getPassword(), HashPassword.getHashPassword(subStr[1]))){
             System.out.println("user login ok");
             return ResponseEntity.ok(user);
@@ -53,34 +49,14 @@ public class LoginController {
 
     @RequestMapping(method = RequestMethod.POST, path="/signup")
     public ResponseEntity<?> createUser(@RequestBody User user) {
-        System.out.println(user.toString());
-        //move to @Service or elsewhere
-        user.setTimestamp(OffsetDateTime.now());
-
-        if(userRepository.readByEmail(user.getEmail())!=null){ //есть ли пользователь с таким имейлом?
-            return ResponseEntity.badRequest().build();
-        }
-        User userCreated = userRepository.create(user);
-        if (userCreated == null) {
-            return ResponseEntity.badRequest().build(); // если юзер по каким-то причинам не создался
-        }
-        System.out.println("send mail");
-        mail.sendCode("https://ourproject.space/use_code?code=", "308ty397f239uopdh3f9p823dh928dhp1280dfh89ph", user.getEmail());
-        return ResponseEntity.noContent().build();
+        return userService.create(user);
     }
 
     @RequestMapping(method = RequestMethod.POST, path="/recovery")
-    public ResponseEntity<?> recovery(@RequestBody String email) {
-
+    public ResponseEntity<?> recoveryPassword(@RequestBody String email) {
         System.out.println("recovery");
-        System.out.println(email);
-
-        if(userRepository.readByEmail(email) == null){ //проверка на ниличие в бд
-            return ResponseEntity.notFound().build();
-        } else {
-            System.out.println("send mail");
-            mail.sendCode("https://ourproject.space/use_code?code=", "308ty397f239uopdh3f9p823dh928dhp1280dfh89ph", email);
-        }
-        return ResponseEntity.noContent().build();
+        return userService.recovery(email);
     }
+
+
 }
