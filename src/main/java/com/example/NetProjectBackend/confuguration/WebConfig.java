a20018cc988019f06bd1fd01c980f440c7d91139
MaksimNemoshenkov <nemoshenkov.maxim@gmail.com>
2021-11-16 20:22:44 +0200

    fast fix

--- src/main/java/com/example/NetProjectBackend/confuguration/WebConfig.java
+++ src/main/java/com/example/NetProjectBackend/confuguration/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "https://ourproject.space");
     }
 }
