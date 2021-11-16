2f8964e0fcc0a61ff977f7e89e0db151ddd8e520
Tatiana000 <alice.everdeen.nya@gmail.com>
2021-11-16 14:38:20 +0200

    Added script to fill values to table "client"

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO client (password, first_name, last_name, email, status, role) VALUES
+  ('e1840ff345d52dd3c53a449e7b3b154757861279cbbed51466', 'admin', 'admin', 'admin@gmail.com', 'ACTIVE', 'ADMIN'), -- password=admin
+  ('fd3cac2a804c9497cbbcd62a346dda69fc7e17a89e9138c3aa', 'moder1', 'moder1', 'moder1@gmail.com', 'ACTIVE', 'MODERATOR'), -- password=moder1
+  ('a6a2adeb65a29234a5f72cdb51442aa722893f9711397a54c2', 'moder2', 'moder2', 'moder2@gmail.com', 'ACTIVE', 'MODERATOR'), -- password=moder2
+  ('a7bbb87f21a490564dc8207541596bc2e992c7578435e00b5a', 'user1', 'user1', 'user1@gmail.com', 'ACTIVE', 'USER'), -- password=user1
+  ('f4a40aeea48fdf7f2654630ad7573cfbb51af400c844705c05',  'user2', 'user2', 'user2@gmail.com', 'ACTIVE', 'USER'); -- password=user2
+
