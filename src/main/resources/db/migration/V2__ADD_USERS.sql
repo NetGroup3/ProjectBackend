393f75971a68d68598aaccf9e9bea9e97d6d3d38
Darii Kinash <darkokinash@gmail.com>
2021-11-22 13:49:01 +0200

    Fix to migration

--- src/main/resources/db/migration/V2__ADD_USERS.sql
+++ src/main/resources/db/migration/V2__ADD_USERS.sql
@@ -1,6 +1,6 @@
 INSERT INTO client (password, first_name, last_name, email, status, role) VALUES
-  ('e1840ff345d52dd3c53a449e7b3b154757861279cbbed51466', 'John',  'Doe',        'admin@gmail.com',  'ACTIVE', 'ADMIN'),     -- password=admin
-  ('fd3cac2a804c9497cbbcd62a346dda69fc7e17a89e9138c3aa', 'Katy',  'Perry',      'moder1@gmail.com', 'ACTIVE', 'MODERATOR'), -- password=moder1
-  ('a6a2adeb65a29234a5f72cdb51442aa722893f9711397a54c2', 'Joe',   'Biden',      'moder2@gmail.com', 'ACTIVE', 'MODERATOR'), -- password=moder2
-  ('a7bbb87f21a490564dc8207541596bc2e992c7578435e00b5a', 'Taras', 'Shevchenko', 'user1@gmail.com',  'ACTIVE', 'USER'),      -- password=user1
-  ('f4a40aeea48fdf7f2654630ad7573cfbb51af400c844705c05', 'Lesia', 'Ukrainka',   'user2@gmail.com',  'ACTIVE', 'USER');      -- password=user2
+  ('$2a$10$gR2B.htZjygoaQ8YIEP8zOBY5UwF/NU8UkeTRsYcf05MTIdAt898S', 'John',  'Doe',        'admin@gmail.com',  'ACTIVE', 'ADMIN'),     -- password=admin
+  ('$2a$10$IMhAKD/jwFvm942e0eTq9.uX4GQD9k/b29QiQ/o0VKjZV1/l7.1sa', 'Katy',  'Perry',      'moder1@gmail.com', 'ACTIVE', 'MODERATOR'), -- password=moder1
+  ('$2a$10$zbsZhuYaKtS003Yy4RYzm.yqdgSIA.BTGz8gi843oUURPZART423u', 'Joe',   'Biden',      'moder2@gmail.com', 'ACTIVE', 'MODERATOR'), -- password=moder2
+  ('$2a$10$mw/ql5RRGajQ0jYinJvBR.a.fRbjXzr7Cr9xv6cM78mIihVDf42s2', 'Taras', 'Shevchenko', 'user1@gmail.com',  'ACTIVE', 'USER'),      -- password=user1
+  ('$2a$10$rJAsOebtWEf.emM5t5Pdqe5cPs/vrjDDQyEq7j88YbXo5iVoN6HKu', 'Lesia', 'Ukrainka',   'user2@gmail.com',  'ACTIVE', 'USER');      -- password=user2
