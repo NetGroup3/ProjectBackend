189f61d551183f2ac187292466205e21805e03c0
Tatiana000 <alice.everdeen.nya@gmail.com>
2021-11-23 16:01:10 +0200

    Added values for tables dish_ingredient, dish_kitchenware, event_dish, event_ingredient, event_member, favourite, friend, like, message, message_status, post, stock, comment, wish_list.

--- src/main/resources/db/migration/V2__ADD_USERS.sql
+++ src/main/resources/db/migration/V2__ADD_USERS.sql
@@ -11,8 +11,10 @@ VALUES ('First event', 'First event', 'ACTIVE'),
        ('Third event', 'Third event', 'ACTIVE');
 
 INSERT INTO ingredient (title, description, category, is_active, measurement)
-VALUES ('Milk', 'Cow''s milk', 'Milk product', TRUE, '0.3 liter'),
-       ('Ice Cream', 'Vanilla Ice Cream', 'Milk product', TRUE, '0.1 liter');
+VALUES ('Milk', 'Cow''s milk', 'Milk product', TRUE, 'liter'),
+       ('Ice Cream', 'Vanilla Ice Cream', 'Milk product', TRUE, 'gram'),
+       ('Vanilla sugar', 'Cow''s milk', 'Milk product', TRUE, 'gram'),
+       ('Strawberry', 'Cow''s milk', 'Milk product', TRUE, 'gram');
 
 INSERT INTO dish (title, description, category, receipt, is_active, amount)
 VALUES ('Milkshake', 'Cool sweet drink', 'Non-alcoholic drinks', 'Milk, ice cream, vanilla sugar', TRUE, 2),
@@ -22,3 +24,82 @@ INSERT INTO kitchenware (title, description, category, is_active)
 VALUES ('Glass', 'Glass', 'Dish', TRUE),
        ('Mixer', 'Mixer', 'Cooking tool', TRUE),
        ('Spoon', 'Spoon', 'Cooking tool', TRUE);
+
+INSERT INTO dish_ingredient (dish_id, ingredient_id, ingredient_amount)
+VALUES (1, 1, 0.3),
+       (1, 2, 300),
+       (1, 3, 50),
+       (2, 1, 0.3),
+       (2, 2, 300),
+       (2, 3, 50),
+       (2, 4, 200);
+
+INSERT INTO dish_kitchenware (dish_id, kitchenware_id)
+VALUES ( 1, 1),
+       ( 1, 2),
+       ( 2, 1),
+       ( 2, 2);
+
+INSERT INTO event_dish (user_id, event_id, dish_id, quantity)
+VALUES (4, 1, 1, 5),
+       (4, 1, 2, 5),
+       (5, 2, 1, 10),
+       (5, 3, 2, 8);
+
+INSERT INTO event_ingredient (user_id, event_id, ingredient_id, amount)
+VALUES (4, 1, 1, 10),
+       (4, 1, 2, 10),
+       (4, 1, 3, 10),
+       (4, 1, 4, 5),
+       (5, 2, 1, 10),
+       (5, 2, 2, 10),
+       (5, 2, 3, 10),
+       (5, 3, 1, 8),
+       (5, 3, 2, 8),
+       (5, 3, 3, 8),
+       (5, 3, 4, 8);
+
+INSERT INTO event_member (user_id, event_id, status, is_owner)
+VALUES ( 4, 1, 'ACTIVE', TRUE),
+       ( 5, 1, 'ACTIVE', TRUE),
+       ( 4, 2, 'ACTIVE', TRUE),
+       ( 5, 2, 'ACTIVE', TRUE),
+       ( 4, 3, 'ACTIVE', TRUE),
+       ( 5, 3, 'ACTIVE', TRUE);
+
+INSERT INTO favourite (user_id, dish_id)
+VALUES (4, 1),
+       (5, 2);
+
+INSERT INTO friend (sender_id, recepient_id, status)
+VALUES (4, 5, 'ACCEPT');
+
+INSERT INTO "like" (user_id, dish_id)
+VALUES (4, 1),
+       (5, 2);
+
+INSERT INTO message (user_id, event_id, text)
+VALUES (5, 1, 'Good job');
+
+INSERT INTO message_status (user_id, message_id, status)
+VALUES (5, 1, 'ACTIVE');
+
+INSERT INTO post (user_id, title, content)
+VALUES (4, 'First post', 'Post''s content'),
+       (5, 'Second post', 'Post''s content');
+
+INSERT INTO stock (user_id, ingredient_id, amount)
+VALUES (4, 1, 3),
+       (4, 2, 3),
+       (4, 3, 3),
+       (4, 4, 2),
+       (5, 1, 5),
+       (5, 3, 6);
+
+INSERT INTO comment (user_id, dish_id, text)
+VALUES (4, 1, 'Delicious Milkshake'),
+       (5, 2, 'Delicious Strawberry Milkshake');
+
+INSERT INTO wish_list (user_id, ingredient_id, event_id, amount)
+VALUES (4, 4, 1, 1);
+
