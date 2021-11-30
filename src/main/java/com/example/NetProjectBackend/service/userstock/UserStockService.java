0407a402f3a2db3d69e4ac2b393dbddc53715a6f
Greg <realgregorygrytsyniak@gmail.com>
2021-11-30 16:44:06 +0200

    body realstock

--- src/main/java/com/example/NetProjectBackend/service/userstock/UserStockService.java
+++ src/main/java/com/example/NetProjectBackend/service/userstock/UserStockService.java
@@ -1,9 +1,12 @@
 package com.example.NetProjectBackend.service.userstock;
 
+import com.example.NetProjectBackend.models.UserStockElement;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface UserStockService {
-    ResponseEntity<?> readStock(int userId, int limit, int offset);
+    List<UserStockElement> readStock(int userId, int limit, int offset);
 
     ResponseEntity<?> deleteStockElement(int userId, String ingredient);
 
