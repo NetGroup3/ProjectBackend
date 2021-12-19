1aa737b427edc0a1a9152aaab6efb4a10ca15258
Мария Куржумова <kurzhumova@ukr.net>
2021-12-19 20:50:48 +0200

    events

--- src/main/java/com/example/NetProjectBackend/service/jwt/AuthTokenFilter.java
+++ src/main/java/com/example/NetProjectBackend/service/jwt/AuthTokenFilter.java
@@ -47,7 +47,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         String headerAuth = request.getHeader("Authorization");
 
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
-            return headerAuth.substring(7);
+            return headerAuth.substring(7, headerAuth.length());
         }
 
         return null;
